3f433538486714609be11931f01caa36d5808bf6
flip.ua <flip.ua71@gmail.com>
2018-10-04 23:37:47 +0300

    add FB authorization

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/login/facebook', 'Auth\LoginController@redirectToFacebookProvider');
 
