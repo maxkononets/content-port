076062fb95244c6b8b28f28bf6d5a3f3caf0f06d
flip.ua <flip.ua71@gmail.com>
2018-10-04 22:35:09 +0300

    add FB authorization

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,20 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
     use AuthenticatesUsers;
 
@@ -35,5 +28,40 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Redirect the user to the Facebook authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToFacebookProvider()
+    {
+        return Socialite::driver('facebook')->scopes([
+            ])->redirect();
+    }
+
+    /**
+     * Obtain the user information from Facebook.
+     *
+     * @return void
+     */
+    public function handleProviderFacebookCallback()
+    {
+        $auth_user = Socialite::driver('facebook')->user();
+
+        $user = User::updateOrCreate(
+            [
+                'email' => $auth_user->email
+            ],
+            [
+                'token' => $auth_user->token,
+                'name'  =>  $auth_user->name,
+                'password' => bcrypt(str_random(20))
+            ]
+        );
+
+        Auth::login($user, true);
+        return redirect()->to('/'); // Redirect to a secure page
     }
 }
