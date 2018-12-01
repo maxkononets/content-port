aefb0d7f1890e6fb5c3d62ee18db65c97902b3b1
maxkononets <maxym.kononets@gmail.com>
2018-12-01 16:55:59 +0200

    [+] fix bags

--- app/Services/Facebook/FacebookPostService.php
+++ app/Services/Facebook/FacebookPostService.php
@@ -10,12 +10,16 @@ namespace App\Services\Facebook;
 
 
 use App\Category;
+use App\FacebookAccount;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
 use Facebook\Facebook;
 use Illuminate\Support\Facades\Auth;
 
 class FacebookPostService
 {
     public $facebook;
+    const COUNT_POSTS_PER_PAGE = 100;
 
     /**
      * FacebookPostService constructor.
@@ -24,7 +28,6 @@ class FacebookPostService
     public function __construct(Facebook $fb)
     {
         $this->facebook = $fb;
-        define('COUNT_POSTS_PER_PAGE', 100);
     }
 
     /**
@@ -39,7 +42,7 @@ class FacebookPostService
         $groups = $category->groups;
         if($groups->isNotEmpty()) {
             $fbAccount = Auth::user()->facebookAccounts->first();
-            $countPostPerRequest = round(COUNT_POSTS_PER_PAGE / count($groups));
+            $countPostPerRequest = round(self::COUNT_POSTS_PER_PAGE / count($groups));
             $groupsIds = $groups->pluck('id');
             $batchRequests = $groupsIds->map(function ($item) use ($fbAccount, $countPostPerRequest) {
                 return $this->facebook->request(
@@ -67,10 +70,6 @@ class FacebookPostService
             return json_decode($item, true)['feed']['data'];
         })->collapse()->toArray();
         $posts = $this->pluckPostData($postsResponse);
-//
-//        $paging = collect($postData->getDecodedBody())->pluck('body')->map(function ($item){
-//            return json_decode($item, true)['feed']['paging'];
-//        })->toArray();
         return [
             'posts' => $posts,
 //            'paging' => $paging,
@@ -106,4 +105,119 @@ class FacebookPostService
         return $prepareData;
     }
 
+    /**
+     * @param $type_group
+     * @param $page_id
+     * @param $text
+     * @param $images
+     * @param $videos
+     * @param $facebookAccountId
+     */
+    public function sendFBPost(
+        $type_group,
+        $page_id,
+        $text,
+        $images,
+        $videos,
+        $facebookAccountId
+    ){
+        switch ($type_group) {
+
+            case 'page':
+                $token = $this->getPageToken($page_id, $facebookAccountId);
+
+
+                break;
+            case 'group':
+                $token = FacebookAccount::find($facebookAccountId)->token;
+
+        }
+
+        if (isset($videos[0])) {
+            $this->publishVideo($page_id, $text, $videos, $token);
+        } else {
+            $this->publishPost($page_id, $text, $images, $token);
+        }
+    }
+
+
+    public function getPageToken($page_id, $facebookAccountId)
+    {
+        $facebookAccount = FacebookAccount::find($facebookAccountId);
+        try {
+            $request = $this->facebook->get('/' . $page_id . '/?fields=access_token',
+                $facebookAccount->token)->getDecodedBody();
+            $page_token = $request['access_token'];
+            return $page_token;
+        } catch (FacebookSDKException $e) {
+            info($e);
+        }
+
+    }
+
+    public function uploadPhotoToFB($images, $page_id, $token)
+    {
+        $photoIdArray = array();
+        foreach ($images as $photoURL) {
+            $params = array(
+                "url" => $photoURL,
+                "published" => false
+            );
+            try {
+                $postResponse = $this->facebook->post('/' . $page_id . '/photos', $params, $token);
+                $photoId = $postResponse->getDecodedBody();
+                if (!empty($photoId["id"])) {
+                    $photoIdArray[] = $photoId["id"];
+                }
+            } catch (FacebookResponseException $e) {
+                exit();
+            } catch (FacebookSDKException $e) {
+                exit();
+            }
+        }
+        return $photoIdArray;
+
+    }
+
+    public function publishVideo($page_id, $text, $videos, $token)
+    {
+
+        $post = array(
+            'description' => $text,
+            'source' => $this->facebook->videoToUpload(implode($videos))
+        );
+        try {
+            $request = $this->facebook->post('/' . $page_id . '/videos',
+                $post,
+                $token);
+
+            $request = $request->getGraphNode()->asArray();
+        } catch (FacebookSDKException $e) {
+            info($e);
+        }
+
+    }
+
+
+    public function publishPost($page_id, $text, $images, $token)
+    {
+        $images_id = $this->uploadPhotoToFB($images, $page_id, $token);
+
+        $post = array('message' => $text);
+
+        foreach ($images_id as $k => $image) {
+            $post['attached_media'][$k] = '{"media_fbid":"' . $image . '"}';
+        };
+        try {
+            $request = $this->facebook->post('/' . $page_id . '/feed',
+                $post,
+                $token);
+
+            $request = $request->getGraphNode()->asArray();
+            //info($request);
+        } catch (FacebookSDKException $e) {
+            info($e);
+        }
+
+    }
 }
